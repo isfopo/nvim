81b743153673158fadfdf1ac9d395f8dca784cac
Isaac Poole <isaacpoolemusic@gmail.com>
2025-11-11 19:03:01 -0500

    sets theme to rose-pine-moon

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -15,7 +15,7 @@ require("lazy").setup({
   { import = "plugins" },
 } --[[@as LazySpec]], {
   -- Configure any other `lazy.nvim` configuration options here
-  install = { colorscheme = { "astrotheme", "habamax" } },
+  install = { colorscheme = { "rose-pine-moon", "habamax" } },
   ui = { backdrop = 100 },
   performance = {
     rtp = {
