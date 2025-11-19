0db9d51d0c49d93b53ad5fe392a0f306f9b1b461
Isaac Poole <isaacpoolemusic@gmail.com>
2025-11-19 18:51:23 -0500

    adds ai completion

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,7 +5,7 @@ return {
   "nvimtools/none-ls.nvim",
   opts = function(_, opts)
     -- opts variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
+    local null_ls = require "null-ls"
 
     -- Check supported formatters and linters
     -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -14,9 +14,13 @@ return {
     -- Only insert new sources, do not replace the existing ones
     -- (If you wish to replace, use `opts.sources = {}` instead of the `list_insert_unique` function)
     opts.sources = require("astrocore").list_insert_unique(opts.sources, {
+      null_ls.builtins.completion.spell,
       -- Set a formatter
-      -- null_ls.builtins.formatting.stylua,
-      -- null_ls.builtins.formatting.prettier,
+      null_ls.builtins.formatting.stylua,
+      null_ls.builtins.formatting.prettier,
+      -- Set a linter
+      null_ls.builtins.diagnostics.yamllint,
+      null_ls.builtins.diagnostics.pylint,
     })
   end,
 }
