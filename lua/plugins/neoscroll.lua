7147218d1b01fcd00a00bdde1fd1278e0349444f
Isaac Poole <isaacpoolemusic@gmail.com>
2025-11-21 19:18:21 -0500

    adds mappings

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -2,5 +2,16 @@ return {
   "karb94/neoscroll.nvim",
   opts = {
     easing = "quartic",
+    mappings = { -- disable Ctrl+e mapping to avoid conflict with scnvim
+      "<C-u>",
+      "<C-d>",
+      "<C-b>",
+      "<C-f>",
+      "<C-y>",
+      -- "<C-e>", -- disabled to avoid conflict with scnvim
+      "zt",
+      "zz",
+      "zb",
+    },
   },
 }
