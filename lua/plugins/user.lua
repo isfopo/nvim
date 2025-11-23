fba50ad542b6b969e2481888ba113e5868ad109d
Isaac Poole <isaacpoolemusic@gmail.com>
2025-11-22 22:44:15 -0500

    removes presence plugin

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,21 +1,7 @@
 -- You can also add or configure plugins by creating files in this `plugins/` folder
--- PLEASE REMOVE THE EXAMPLES YOU HAVE NO INTEREST IN BEFORE ENABLING THIS FILE
--- Here are some examples:
 
 ---@type LazySpec
 return {
-
-  -- == Examples of Adding Plugins ==
-
-  "andweeb/presence.nvim",
-  {
-    "ray-x/lsp_signature.nvim",
-    event = "BufRead",
-    config = function() require("lsp_signature").setup() end,
-  },
-
-  -- == Examples of Overriding Plugins ==
-
   -- customize dashboard options
   {
     "folke/snacks.nvim",
@@ -196,6 +182,4 @@ return {
       )
     end,
   },
-
-
 }
